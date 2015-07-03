69c57b901f76ae0045cbcbae5301729bf0235ba5
Matt Ridgway <matt.ridgway@live.co.uk>
2015-07-03 16:14:45 +0100

    Added result model

---
+++ src/Weather.Bbc/Models/WeatherResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Weather.Bbc.Models
+{
+    public class WeatherResult
+    {
+        public string Location { get; private set; }
+        public double TemperatureCelsius { get; private set; }
+        public double WindSpeedKph { get; private set; }
+
+        public WeatherResult(string location, double temperatureCelsius, double windSpeedKph)
+        {
+            Location = location;
+            TemperatureCelsius = temperatureCelsius;
+            WindSpeedKph = windSpeedKph;
+        }
+    }
+}
