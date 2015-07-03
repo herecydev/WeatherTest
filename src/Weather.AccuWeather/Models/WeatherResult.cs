74f5d96242c942927a2359f6768bdfe41829a7d7
Matt Ridgway <matt.ridgway@live.co.uk>
2015-07-03 16:44:34 +0100

    Added accuweather result

---
+++ src/Weather.AccuWeather/Models/WeatherResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Weather.AccuWeather.Models
+{
+    public class WeatherResult
+    {
+        public string Location { get; private set; }
+        public double TemperatureFahrenheit { get; private set; }
+        public double WindSpeedMph { get; private set; }
+
+        public WeatherResult(string location, double temperatureFahrenheit, double windSpeedMph)
+        {
+            Location = location;
+            TemperatureFahrenheit = temperatureFahrenheit;
+            WindSpeedMph = windSpeedMph;
+        }
+    }
+}
