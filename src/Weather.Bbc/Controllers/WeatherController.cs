81887fdf8ac29438bf65936ba67105edae190421
Matt Ridgway <matt.ridgway@live.co.uk>
2015-07-03 16:38:32 +0100

    Added bbc weather provider

---
+++ src/Weather.Bbc/Controllers/WeatherController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using Weather.Bbc.Models;
+
+namespace Weather.Bbc.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        private Random _rng;
+
+        public WeatherController()
+        {
+            _rng = new Random();
+        }
+
+        [HttpGet]
+        public WeatherResult Get(string location) 
+            => new WeatherResult(
+                location,
+                _rng.Next(0, 38),
+                _rng.Next(0, 32)
+            );
+    }
+}
