4ab7a09c674691385aa2279e22f0084b0db868b3
Matt Ridgway <matt.ridgway@live.co.uk>
2015-07-03 17:00:40 +0100

    Added accuweather provider

---
+++ src/Weather.AccuWeather/Controllers/WeatherController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using Weather.AccuWeather.Models;
+
+namespace Weather.AccuWeather.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        private Random _rng;
+
+        public WeatherController()
+        {
+            _rng = new Random();
+        }
+
+        [HttpGet]
+        public WeatherResult Get(string location)
+            => new WeatherResult(
+                location,
+                _rng.Next(32, 100),
+                _rng.Next(0, 20)
+            );
+    }
+}
